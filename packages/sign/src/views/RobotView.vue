c801077fd8e2ea7092c1ff17d542eafc49f7bdbe
tangqingning <tangqingning@aobi.com>
2022-12-10 16:31:58 +0800

    修复机器人页面的逻辑

--- packages/sign/src/views/RobotView.vue
+++ packages/sign/src/views/RobotView.vue
@@ -1,80 +1,91 @@
 <script lang="ts" setup>
-  import {
-    get,
-    post
-  } from '@/utils/http';
-  import {
-    getBaseInfo
-  } from '@circle/sdk';
-  import {
-    ElButton,
-    ElDivider,
-    ElMessage,
-    ElTag,
-    ElTooltip
-  } from 'element-plus';
-  import {
-    onMounted,
-    reactive
-  } from 'vue';
-  import {
-    useRouter
-  } from 'vue-router';
-  let username = ""
-  const list = reactive([{
-    id: "12",
-    userId: "string",
-    robotNickname: "i am robot",
-    serverName: "英雄联盟",
-    channelName: "疾风剑豪",
-    webhook: "string",
-  }])
-  /**
-   * create onAdd function
-   */
-  const onAdd = () => {
-    console.log('onAdd')
-  }
-
-  onMounted(() => {
-    getBaseInfo().then((res) => {
-      username = res.userInfo.username
-      get('/api/ext/ext/robot/listRobots', {
-        username
-      }).then(res => {
-        console.log(res)
-      }).catch(res => {
-        ElMessage.warning(res.msg || '网络异常, 获取机器人列表失败')
-      })
+import {
+  get,
+  post
+} from '@/utils/http';
+import {
+  getBaseInfo
+} from '@circle/sdk';
+import {
+  ElButton,
+  ElDivider,
+  ElMessage,
+  ElTag
+} from 'element-plus';
+import {
+  onMounted, ref,
+  type Ref
+} from 'vue';
+import {
+  useRouter
+} from 'vue-router';
+let username = ""
+const list: Ref<any[]> = ref([])
+/**
+ * create onAdd function
+ */
+const onAdd = () => {
+  // todo 弹二级窗口填写机器人名称，点击按钮创建
+  getBaseInfo().then((res) => {
+    const serverId = res.currentChannelInfo.serverId
+    const channelId = res.currentChannelInfo.channelId
+    post('/ext/robot/create', {
+      username,
+      serverId,
+      channelId,
+      robotName: 'robotName'
+    }).then(res => {
+      getRobotList()
+    }).catch(res => {
+      ElMessage.warning(res.msg || '网络异常, 创建机器人失败')
     })
   })
+}
 
-  //todo: 打开详情，展示webhook地址，提供一个推送消息到群的示例
-  const router = useRouter()
-  const openDetailView = (id: string) => {
-    router.push({
-      path: '/ext/robot/detail',
-      query: {
-        id
+onMounted(() => {
+  getBaseInfo().then((res) => {
+    username = res.userInfo.username
+    getRobotList()
+  })
+})
+
+const getRobotList = () => {
+  get('/ext/robot/listRobots', {
+    username
+  }).then((res: any) => {
+    console.log(res.value.list)
+    list.value = res.value.list
+  }).catch(res => {
+    ElMessage.warning(res.msg || '网络异常, 获取机器人列表失败')
+  })
+}
+
+//todo: 打开详情，展示webhook地址，提供一个推送消息到群的示例
+const router = useRouter()
+const openDetailView = (id: string) => {
+  router.push({
+    path: '/ext/robot/detail',
+    query: {
+      id
+    }
+  })
+}
+
+// todo: 重置webhook地址
+const resetKey = (id: number) => {
+  post(`/ext/robot/resetKey`, {
+    id,
+    username
+  }).then((res: any) => {
+    list.forEach((v: any) => {
+      if (v.id === res.robot.id) {
+        v.webhook = res.robot.webhook
       }
     })
-  }
-
-  // todo: 重置webhook地址
-  const resetKey = (id: number) => {
-    post(`/api/ext/robot/resetKey`, {
-      id,
-      username
-    }).then((res: any) => {
-      list.forEach(v => {
-        if (v.id === res.robot.id) {
-          v.webhook = res.robot.webhook
-        }
-      })
-    }).catch(res => {
-      ElMessage.warning(res.msg || '网络异常, 重置webhook地址失败')
-    })
-  }
+  }).catch(res => {
+    ElMessage.warning(res.msg || '网络异常, 重置webhook地址失败')
+  })
+}
 </script>
 
 <template>
@@ -91,10 +102,10 @@
         
       </el-tooltip> -->
       <div class="title">
-          <div style="margin-right:10px;font-size:larger">{{ item.robotNickname }}</div>
-          <el-tag type="info">BOT</el-tag>
-        </div>
-        <div>正在 [社区-{{ item.serverName }} 频道-{{ item.channelName }}] 中使用</div>
+        <div style="margin-right:10px;font-size:larger">{{ item.robotNickname }}</div>
+        <el-tag type="info">BOT</el-tag>
+      </div>
+      <div>正在 [社区-{{ item.serverName }} 频道-{{ item.channelName }}] 中使用</div>
       <el-divider />
     </div>
   </div>
@@ -106,30 +117,31 @@
   width: 100%;
   text-align: center;
 }
-  .title {
-    display: flex;
-  }
 
-  .tooltip-base-box {
-    width: 600px;
-  }
+.title {
+  display: flex;
+}
 
-  .tooltip-base-box .row {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-  }
+.tooltip-base-box {
+  width: 600px;
+}
 
-  .ext__robot-item {
-    cursor: pointer;
-  }
+.tooltip-base-box .row {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
 
-  .tooltip-base-box .center {
-    justify-content: center;
-  }
+.ext__robot-item {
+  cursor: pointer;
+}
 
-  .tooltip-base-box .box-item {
-    width: 110px;
-    margin-top: 10px;
-  }
+.tooltip-base-box .center {
+  justify-content: center;
+}
+
+.tooltip-base-box .box-item {
+  width: 110px;
+  margin-top: 10px;
+}
 </style>
