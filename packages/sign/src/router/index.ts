f340eeb14b386ad5c0f68348646fee1af2709fc4
chenyuankun <chenyuankun@nowcoder.com>
2022-12-20 15:40:36 +0800

    fix: fix build error

--- packages/sign/src/router/index.ts
+++ packages/sign/src/router/index.ts
@@ -1,10 +1,10 @@
 import { useUserStore } from '@/stores/user'
 import { getBaseInfo } from '@circle/sdk'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import ReportShareView from '../views/HomeView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
